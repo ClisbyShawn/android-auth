1db8ffb7c19b638dcb8857644d0d066e4d9864de
Shawn Clisby <shawn.clisby@gmail.com>
2021-03-08 20:00:45 -0500

    Updated dependencies, upgraded Kotlin version, AuthService.kt returns a Retrofit Response that wraps a String and User object, in AuthRespository checking for response success and null check of server response data.Adds better error and success handling.

--- androidAuth/src/main/java/com/android/shawnclisby/androidauth/network/endpoints/AuthService.kt
+++ androidAuth/src/main/java/com/android/shawnclisby/androidauth/network/endpoints/AuthService.kt
@@ -1,6 +1,7 @@
 package com.android.shawnclisby.androidauth.network.endpoints
 
 import com.android.shawnclisby.androidauth.models.User
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -8,8 +9,8 @@ import retrofit2.http.POST
 interface AuthService {
 
     @POST("login")
-    suspend fun login(@Body user: Map<String, String>): String
+    suspend fun login(@Body user: Map<String, String>): Response<String>
 
     @GET("me")
-    suspend fun me(): User
+    suspend fun me(): Response<User>
 }
